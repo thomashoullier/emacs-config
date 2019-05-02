a63369d23f8da7b279b6dd1058188ad54fd770df
Karl <XXX>
2019-05-02 21:24:52 +0200

    added slime-autodoc contrib

--- custom/slime-conf.el
+++ custom/slime-conf.el
@@ -3,6 +3,9 @@
 ;;; Settings
 (setq inferior-lisp-program "~/.emacs.d/sbcl-core/sbcl-init.sh")
 ;;; Loading slime contributions
-(setq slime-contribs '(slime-repl))
+(setq slime-contribs '(slime-repl slime-autodoc))
+;;; Activating autodoc in Common-lisp modes
+(add-hook 'lisp-mode-hook 'slime-autodoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'slime-autodoc-mode)
 ;;; Launching SLIME
 (slime)
