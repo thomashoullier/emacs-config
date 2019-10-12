1cdc07fd311cbcbf6448045ad9bf9cee33362b43
Karl <a>
2019-10-12 16:31:36 +0200

    changed asdf link folder location

--- sbcl-core/generate-core.lisp
+++ sbcl-core/generate-core.lisp
@@ -1,4 +1,4 @@
 (mapc 'require '(sb-bsd-sockets sb-posix sb-introspect sb-cltl2 asdf))
-(push #p"~/repos/asdf-links/" asdf:*central-registry*)
+(push #p"~/repos/cl/asdf-links/" asdf:*central-registry*)
 
 (save-lisp-and-die "sbcl.core-for-slime")
